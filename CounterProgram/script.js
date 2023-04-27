e316f11fd6bd223f764f05380b787246f7fa85b9
pmging <125826032+pmging@users.noreply.github.com>
2023-04-27 19:38:04 +0800

    Add files via upload

---
+++ CounterProgram/script.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+document.getElementById("decBtn").onclick = function() {
+    count -= 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("rstBtn").onclick = function() {
+    count = 0;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("incBtn").onclick = function() {
+    count += 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
